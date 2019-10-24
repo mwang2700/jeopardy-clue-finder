32e652bae9feae98355d9662f88b0cf426f04f0b
maxwa <maxwang2700@gmail.com>
2019-10-24 00:12:10 -0400

    Updates

---
+++ precache-manifest.517b2ff0dda0006663131e3c2a623947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ea6360da39b8dfbf404751188b4551c",
+    "url": "/jeopardy-clue-finder/index.html"
+  },
+  {
+    "revision": "3454c506e4e2ade919c9",
+    "url": "/jeopardy-clue-finder/static/css/2.d5f11bc3.chunk.css"
+  },
+  {
+    "revision": "691f8aa85dde616e29ca",
+    "url": "/jeopardy-clue-finder/static/css/main.95a2ebeb.chunk.css"
+  },
+  {
+    "revision": "3454c506e4e2ade919c9",
+    "url": "/jeopardy-clue-finder/static/js/2.5545af1c.chunk.js"
+  },
+  {
+    "revision": "691f8aa85dde616e29ca",
+    "url": "/jeopardy-clue-finder/static/js/main.0a5c6362.chunk.js"
+  },
+  {
+    "revision": "8a8ca1ea53304ec121ed",
+    "url": "/jeopardy-clue-finder/static/js/runtime-main.478fac5e.js"
+  }
+]);
